4f937423bd35d44d35e7088a06fbed160a3b7eb0
Henry Poma <poma.hny@gmail.com>
2026-02-10 10:59:17 -0400

    dia12

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-console.log(
-    `hola desde prueba.js
-    `
-)
+/*
+let usuario = prompt("Hola usuario, ingresa tu nombre:")
+let edad = prompt("Hola " +usuario + ", ingresa tu edad:")
 
-let nombre
+let peso = prompt("Hola " +usuario + ", ingresa tu peso:")
 
-console.log("2" === 2)
+let altura = prompt("Hola " +usuario + ", ingresa tu altura:")
+
+let indicaMasaCorporal = peso / (altura**2)
+
+alert("tu indice de masa corparal es de: " + indicaMasaCorporal)
+
+*/
+
+let numeros = [5, 12, 8, 3, 15];
+let indice = 0;
+let encontrado = false;
+
+while (indice < numeros.length && !encontrado) {
+  if (numeros[indice] > 10) {
+    console.log(`Primer número mayor a 10: ${numeros[indice]}`);
+    encontrado = true;
+  }
+  indice++;
+}
